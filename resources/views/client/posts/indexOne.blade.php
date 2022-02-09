b6548454810bf97fe2f21c537f52ac5eec15991b
Jozef Bugos <jozef.bugos@sap.com>
2022-02-09 08:47:41 +0100

    showing photo from db not form url

--- resources/views/client/posts/indexOne.blade.php
+++ resources/views/client/posts/indexOne.blade.php
@@ -63,7 +63,13 @@
                                 <tr>
                                     <td class="px-6 py-4">
                                         <div class="flex items-center hover:zoom-11">
-                                            @if($post->img != null)
+                                            @if($post->photo != null)
+                                                <div class="flex-shrink-0 h-10 w-10">
+                                                    <a href="{{ route('showPost', ['politician' => $post->politician(), 'post' => $post]) }}">
+                                                        <img class="h-10 w-10 rounded-full shadow" src="data:image/png;base64,{{ base64_encode($photo) }}" alt="Photo from FB post">
+                                                    </a>
+                                                </div>
+                                            @elseif($post->img != null)
                                                 <div class="flex-shrink-0 h-10 w-10">
                                                     <a href="{{ route('showPost', ['politician' => $post->politician(), 'post' => $post]) }}">
                                                         <img class="h-10 w-10 rounded-full shadow" src="{{ $post->img }}" alt="Photo from FB post">
