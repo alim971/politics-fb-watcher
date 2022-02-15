8cac7ba3869c77ed23b9433b2886c1edc772e3c8
Jozef Bugos <jozef.bugos@sap.com>
2022-02-15 08:03:56 +0100

    create view of blog

--- resources/views/livewire/select.blade.php
+++ resources/views/livewire/select.blade.php
@@ -1,24 +1,34 @@
-<div>
+<div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">
     <div class="form-group">
         <label for="politicianId"
                class="control-label">Politician</label>
-        <select name="politicianId" id="politicianId" class="form-control" wire:click="changeEvent($event.target.value)">
-            <option value="" selected>--</option>
+        <select name="politicianId" id="politicianId" class="form-control" wire:model="selectedPolitician">
+            <option value="">--</option>
             @foreach($politicians as $politician)
-                <option value="{{ $politician->id }}" {{ $politicianId && $politician->id == $politicianId ? 'selected' : '' }}>{{  $politician->fullName() }}</option>
+                <option value="{{ $politician->id }}">{{  $politician->fullName() }}</option>
             @endforeach
         </select>
     </div>
-    @if($politicianId)
+    @if(!is_null($selectedPolitician))
     <div class="form-group">
-        <label for="politicianId"
-               class="control-label">Politician</label>
-        <select name="politicianId" id="politicianId" class="form-control">
-            <option value=""selected>--</option>
-            @foreach($posts as $posts)
-                <option value="{{ $post->id }}" {{ $postId && $post->id == $postId ? 'selected' : '' }} >{{ $post->firstWords(15) }}</option>
+        <label for="postId"
+               class="control-label">Post</label>
+        <select name="postId" id="postId" class="form-control" wire:model="selectedPost" style="max-width: 100%">
+            <option value="">--</option>
+            @php
+                $table = new App\Models\Post;
+                $table->setTable($politicians->find($selectedPolitician)->nick());
+                $posts = $table->get()->sortByDesc('date');
+            @endphp
+            @foreach($posts as $post)
+                <option value="{{ $post->id }}"  >{{ $post->firstWords(15) }}</option>
             @endforeach
         </select>
     </div>
+    @endif
+    @if(!is_null($text))
+        <div class="mt-6 text-gray-500">
+            {!! $text !!}
+        </div>
     @endif
 </div>
