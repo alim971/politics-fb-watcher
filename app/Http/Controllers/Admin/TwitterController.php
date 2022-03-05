f8aff03b8f79b560f6e1ea5c0cdfdb61b9f70eeb
Jozef Bugos <jozef.bugos@sap.com>
2022-03-05 02:45:48 +0100

    adding live ukraine news

--- app/Http/Controllers/Admin/TwitterController.php
+++ app/Http/Controllers/Admin/TwitterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Twitter;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class TwitterController extends Controller
 {
@@ -43,7 +44,7 @@ class TwitterController extends Controller
         $twitter->url = $request->input('url');
         $twitter->nick = $request->input('nick');
         $twitter->save();
-
+        Artisan::call('tweets:watch');
         return redirect()->route('twitter.index');
     }
 
