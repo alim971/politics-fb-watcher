da8a6317af9cbce2ab244ba4c19e06a71fd079f7
Jozef Bugos <jozef.bugos@sap.com>
2022-03-10 16:52:37 +0100

    adding live ukraine news

--- app/Console/Commands/TweetsWatch.php
+++ app/Console/Commands/TweetsWatch.php
@@ -104,7 +104,7 @@ class TweetsWatch extends Command
     }
 
     private function getEmbedded($url) {
-        $response = Http::get($this->embeddedUrl . $url);
+        $response = Http::get($this->embeddedUrl  . '/' . $url);
         $decoded = json_decode($response);
         return $decoded->html;
     }
